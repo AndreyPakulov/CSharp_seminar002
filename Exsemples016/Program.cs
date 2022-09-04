7d382a78ecace077a81703362da3612fb027c0ff
Andrey Pakulov <anpa1984@yandex.ru>
2022-09-04 21:49:48 +0200

    added example 016

---
+++ Exsemples016/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CheckSquare(numberA, numberB));
+
+bool CheckSquare(int num1, int num2)
+{
+    return num1*num1 == num2 || num2*num2 == num1;
+}
